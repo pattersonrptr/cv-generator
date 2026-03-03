cdd5a9ffeebccc14e77a7c8cfc7b659353ae1555
patterson <pattersonjunior@gmail.com>
2026-03-03 13:48:14 -0300

    remove README.old.md

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,33 +1,23 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import '../styles/Footer.css';
 
 const Footer = () => {
   const ano = new Date().getFullYear();
   const iconeGithub = <FontAwesomeIcon icon={faGithub} />;
-  const iconeLinkedin = <FontAwesomeIcon icon={faLinkedin} />;
 
   return (
     <footer>
-      <p className='text'>Adriel Faria, {ano}</p>
+      <p className='text'>Patterson, {ano}</p>
       <div className='icons'>
         <a
-          href='https://github.com/br-adriel'
+          href='https://github.com/pattersonrptr'
           target='_blank'
           rel='noopener noreferrer'
           title='Github'
           alt='Github'
         >
           {iconeGithub}
-        </a>
-        <a
-          href='https://www.linkedin.com/in/adriel-fsantos/'
-          target='_blank'
-          rel='noopener noreferrer'
-          title='LinkedIn'
-          alt='LinkedIn'
-        >
-          {iconeLinkedin}
         </a>
       </div>
     </footer>
